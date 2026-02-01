da6748ae70810bd47b30b41a6da2e1654f4ce2e0
NaifuKishi <gaming@apelmail.ch>
2026-02-01 09:24:53 +0100

    new LibEKL.Tools.Gfx.Pulse

--- tools/gfx.lua
+++ tools/gfx.lua
@@ -20,3 +20,18 @@ function LibEKL.Tools.Gfx.Rotate(frame, angle, scale)
   return m
   
 end
+
+function LibEKL.Tools.Gfx.Pulse(frame, scale)
+
+  local midx = frame:GetHeight() / 2
+  local midy = frame:GetWidth() / 2
+  local m = Libs.Transform2:new()
+  
+  --m:Translate(midx,midy)
+  --m:Rotate(angle)
+  --m:Translate(-midx,-midy)
+  if scale then m:Scale(scale, scale) end
+  
+  return m
+  
+end
